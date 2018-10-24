411dcde878dcd4615b534cff6ff28cb53b5721a9
sak963 <41641650+sak963@users.noreply.github.com>
2018-10-24 10:10:40 -0400

    tangram changes

--- wallet.py
+++ wallet.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 import util
 import db
 import datetime
@@ -5,22 +6,37 @@ import settings
 import asyncio
 import aiohttp
 import socket
+import json
+
+
 
 wallet = settings.wallet
 
 logger = util.get_logger('wallet')
 logger_newuser = util.get_logger('usr', log_file='user_creation.log')
 
-async def communicate_wallet_async(wallet_command):
-	conn = aiohttp.TCPConnector(family=socket.AF_INET6,resolver=aiohttp.AsyncResolver())
-	async with aiohttp.ClientSession(connector=conn) as session:
-		async with session.post("http://{0}:{1}".format(settings.node_ip, settings.node_port),json=wallet_command, timeout=300) as resp:
-			return await resp.json()
+async def communicate_wallet_async_get(wallet_command,action):
+	headers = {"accept": "application/json",
+			   "Authorization": settings.node_pass,
+			   "Content-Type": "application/json"}
+	async with aiohttp.ClientSession() as session:
+		async with session.get("http://{0}:{1}/{2}".format(settings.node_ip, settings.node_port,action),headers=headers, timeout=300) as resp:
+			return await resp.read()
+
+async def communicate_wallet_async(wallet_command,action):
+	headers = {"accept": "application/json",
+			   "Authorization": settings.node_pass,
+			   "Content-Type": "application/json"}
+	async with aiohttp.ClientSession() as session:
+		async with session.post("http://{0}:{1}/{2}".format(settings.node_ip, settings.node_port,action),json=wallet_command,headers=headers, timeout=300) as resp:
+			return await resp.read()
 
 async def get_blocks():
-	wallet_command = {'action':'block_count'}
-	wallet_output = await communicate_wallet_async(wallet_command)
-	return (wallet_output['count'], wallet_output['unchecked'])
+	wallet_command = {}
+	action = "explorer/blocks/unsettled/count"
+	outputText = await communicate_wallet_async_get(wallet_command,action)
+	wallet_output = json.loads(outputText)
+	return (wallet_output['length'])
 
 async def create_or_fetch_user(user_id, user_name):
 	logger.info('attempting to fetch user %s ...', user_id)
@@ -28,11 +44,21 @@ async def create_or_fetch_user(user_id, user_name):
 	if user is None:
 		logger.info('user %s does not exist. creating new user ...',
 					user_id)
-		wallet_command = {'action': 'account_create', 'wallet': wallet}
-		wallet_output = await communicate_wallet_async(wallet_command)
-		address = wallet_output['account']
-		wallet_command = {'action': 'account_representative_set', 'wallet': wallet, 'account':address, 'representative':settings.representative }
-		await communicate_wallet_async(wallet_command)
+		wallet_command = {'identifier': wallet, 'password':  settings.node_pass}
+		action = "actor/wallet/address"
+		outputText = await communicate_wallet_async(wallet_command,action)
+		wallet_output = json.loads(outputText)
+		address = wallet_output['address']
+		#pulowi claim
+		wallet_command = {'identifier': wallet, 'password':  settings.node_pass,'address':  address,'amount':  100}
+		action = "actor/wallet/reward"
+		outputText = await communicate_wallet_async(wallet_command,action)
+		wallet_output = json.loads(outputText)
+		reward_block = wallet_output['reward_block']
+		logger.info('user Reward complete "reward_block": %s', reward_block)
+		#pulowi por el momento no se necesita
+		#wallet_command = {'action': 'account_representative_set', 'wallet': wallet, 'account':address, 'representative':settings.representative }
+		#await communicate_wallet_async(wallet_command)
 		user = db.create_user(user_id=user_id, user_name=user_name,
 							  wallet_address=address)
 		logger.info('user %s created.', user_id)
@@ -54,16 +80,19 @@ async def get_balance(user):
 			'pending':0}
 	else:
 		logger.info('Fetching balance from wallet for %s', user_id)
-		wallet_command = {'action': 'account_balance',
-				  'account': user.wallet_address}
-		wallet_output = await communicate_wallet_async(wallet_command)
+		wallet_command = {'identifier': wallet, 'password':  settings.node_pass,'address':  user.wallet_address}
+		action = "actor/wallet/balance"
+		outputText = await communicate_wallet_async(wallet_command,action)
+		wallet_output = json.loads(outputText)
 		if 'balance' not in wallet_output:
 			# Ops
 			return None
-		actual_balance = int(wallet_output['balance'])
-		pending_balance = int(wallet_output['pending'])
-		actual_balance = (actual_balance / util.RAW_PER_BAN) if settings.banano else (actual_balance / util.RAW_PER_RAI)
-		pending_balance = (pending_balance / util.RAW_PER_BAN) if settings.banano else (pending_balance / util.RAW_PER_RAI)
+		actual_balance = int(Decimal(wallet_output['balance']))
+		#pulowi pendinte
+		#pending_balance = int(wallet_output['pending'])
+		pending_balance = 0
+		actual_balance = (actual_balance)
+		pending_balance = (pending_balance)
 		return {'actual':int(actual_balance),
 			'available': int(actual_balance) - user.pending_send,
 			'pending_send': user.pending_send,
@@ -74,19 +103,18 @@ async def make_transaction_to_address(source_user, amount, withdraw_address, uid
 	# Do not validate address for giveaway tx because we do not know it yet
 	if verify_address:
 		# Check to see if the withdraw address is valid
-		wallet_command = {'action': 'validate_account_number',
-				  'account': withdraw_address}
-		address_validation = await communicate_wallet_async(wallet_command)
+		#pulowi falta servicio de validacion
+		#wallet_command = {'action': 'validate_account_number',
+		#		  'account': withdraw_address}
+		#address_validation = await communicate_wallet_async(wallet_command)
 
-		if settings.banano:
-			if ((withdraw_address[:4] == 'ban_' and len(withdraw_address) != 64)
-			    or address_validation['valid'] != '1'):
-				raise util.TipBotException('invalid_address')
-		else:
-			if (((withdraw_address[:4] == 'xrb_' and len(withdraw_address) != 64)
-			    or (withdraw_address[:5] == 'nano_' and len(withdraw_address) != 65))
-			    or address_validation['valid'] != '1'):
-				raise util.TipBotException('invalid_address')
+		#if ((withdraw_address[:4] == 'ban_' and len(withdraw_address) != 64)
+		#		or address_validation['valid'] != '1'):
+		#	raise util.TipBotException('invalid_address')
+
+		if ((withdraw_address[:4] == 'tgm_' and len(withdraw_address) != 106)):
+			raise util.TipBotException('invalid_address')
+
 
 	amount = int(amount)
 	if amount >= 1:
